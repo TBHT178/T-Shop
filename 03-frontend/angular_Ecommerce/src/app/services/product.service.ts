bc69efcfd5f8e6e75564834cf915326325253883
Tran Bao Huyen Tram <tranbaohuyentram96@gmail.com>
2024-06-02 23:10:10 +0700

    Create new component and add route for product details
    Add router links to the product-list-grid.html
    
    Enhance ProductDetailsComponent and update ProductService to call api
    
    Update html page to display product details

--- 03-frontend/angular_Ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular_Ecommerce/src/app/services/product.service.ts
@@ -10,7 +10,6 @@ import { ProductCategory } from '../common/product-category';
 })
 export class ProductService {
 
-
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
@@ -40,7 +39,10 @@ export class ProductService {
     );
   }
 
-
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
 }
 
 
