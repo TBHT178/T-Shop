e2cf6b581b28572fd13a13a85528c47019332786
Tran Bao Huyen Tram <tranbaohuyentram96@gmail.com>
2024-06-16 18:28:16 +0700

    1. Update okta issuer in backend and frontend 2. Create an interceptor 3. Update app.module.ts to reference interceptor

--- 03-frontend/angular_Ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular_Ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
 
     oidc:{
         clientId: '0oahqntpl1h9ClC1I5d7',
-        issuer: 'https://dev-15829471.okta.com',
+        issuer: 'https://dev-15829471.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']  
         // scopes provide access to information about a user
---
+++ 03-frontend/angular_Ecommerce/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,44 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { Observable, from } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next));
+  }
+
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    // Only add an access token for secured endpoints
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      // get access token
+      const accessToken = this.oktaAuth.getAccessToken();
+
+      console.log('Access Token:', accessToken); // Log the access token for debugging
+
+      if (accessToken) {
+        // clone the request and add new header with access token
+        request = request.clone({
+          setHeaders: {
+            Authorization: 'Bearer ' + accessToken,
+            'Content-Type': 'application/json' // Correct header name
+          }
+        });
+      } else {
+        console.error('No access token found'); // Log if access token is not found
+      }
+    }
+
+    return await lastValueFrom(next.handle(request));
+  }
+}
