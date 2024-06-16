bbd7813325e0cba29122f15e4fef29ae8a274ce3
Tran Bao Huyen Tram <tranbaohuyentram96@gmail.com>
2024-06-16 22:58:36 +0700

    1. Generate key and self-signed certificate 2. Run angular with key and self-signed certificate 3. Update backend with new URL 4. Update redirectUri in angular 5. Update Redirect Uris and API Trusted Origins in Okta Dashboard

--- 03-frontend/angular_Ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular_Ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc:{
         clientId: '0oahqntpl1h9ClC1I5d7',
         issuer: 'https://dev-15829471.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']  
         // scopes provide access to information about a user
         // openid: required for authentication requests, profile: user's name, phone, etc, email: user's email
