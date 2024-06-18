a58d76465f73ebb17ae2b50c272db6f920d46748
Tran Bao Huyen Tram <tranbaohuyentram96@gmail.com>
2024-06-18 20:39:16 +0700

    Add billing details and description for Stripe

--- 02-backend/spring-boot-ecommerce/src/main/java/com/tbht/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tbht/ecommerce/service/CheckoutServiceImpl.java
@@ -74,6 +74,7 @@ public class CheckoutServiceImpl implements CheckoutService{
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "Tramtbh Spring Boot Project purchase");
 
         return PaymentIntent.create(params);
     }
