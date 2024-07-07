35207b65bd07937fb5324916b639354507f4f1f2
Tran Bao Huyen Tram <tranbaohuyentram96@gmail.com>
2024-07-07 19:18:23 +0700

    Fix url of angular to fetch data from springboot

--- 03-frontend/angular_Ecommerce/src/app/services/auth-interceptor.service.ts
+++ 03-frontend/angular_Ecommerce/src/app/services/auth-interceptor.service.ts
@@ -19,7 +19,7 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
     // Only add an access token for secured endpoints
-    const theEndpoint = environment.shopApiUrl + 'orders';
+    const theEndpoint = environment.shopApiUrl + '/orders';
     const securedEndpoints = [theEndpoint];
 
     if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
